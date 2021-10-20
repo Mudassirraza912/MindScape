55a71ec9b27df1f877b81998d771fe2586fdd176
Mudassir Raza <razamudassir912@gmail.com>
2021-10-20 15:26:46 +0500

    first

--- ios/mindscapeTests/mindscapeTests.m
+++ ios/mindscapeTests/mindscapeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeBoilerplateTests : XCTestCase
+@interface mindscapeTests : XCTestCase
 
 @end
 
-@implementation reactNativeBoilerplateTests
+@implementation mindscapeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
