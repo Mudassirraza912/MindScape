67bf0f3c5d50f19ed833d3b29ffdae93892b9400
Yousuf223 <yousuffareed471@gmail.com>
2021-10-21 15:18:57 +0500

    reuseable button

--- src/screens/GetStarted/getStartedMain.js
+++ src/screens/GetStarted/getStartedMain.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
+import Button from '../../components/button'
 
 export const GetstartedMain = ({navigation}) => {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>
-                Get Started Main
-            </Text>
-            <TouchableOpacity onPress={() => navigation.navigate('WalkThrough')}>
-                <Text>
-                    NEXT PAGE
-                </Text>
-            </TouchableOpacity>
+        <View style={styles.container}>
+            <View style={{flex:1,alignItems:"flex-end"}}>
+                <Text>Skip</Text>
+            </View>
+            <Button
+            buttonStyle={{borderColor:"red"}}
+             onPress={() => navigation.navigate('WalkThrough')} title={"Get Started"} textStyle={{color: 'red'}} />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create ({
+    container:{
+        flex:1,
+
+    }
+})
