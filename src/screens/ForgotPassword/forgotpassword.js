7acdcea4e237a369082426ea3748017460b7505b
asifali2002 <asif.bholim90@gmail.com>
2021-10-26 15:52:33 +0500

    create authenticate screen

--- src/screens/ForgotPassword/forgotpassword.js
+++ src/screens/ForgotPassword/forgotpassword.js
@@ -1,18 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { View, Text, StyleSheet, StatusBar,} from 'react-native'
+import { TouchableOpacity,  } from 'react-native-gesture-handler'
 
 export const ForgotPassword = ({navigation}) => {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>
-                Forgot password page
-            </Text>
-            <TouchableOpacity  onPress={() => navigation.navigate('EmailVerification')}>
-                <Text>
-                    NEXT PAGE
+        <>
+        <StatusBar barStyle="dark-content" backgroundColor={'#9493AD'} />
+        <View style={styles.mainContainer}>
+            <View style={styles.ContainerView}>
+                <Text style={{color:"#fff", fontSize:24, textAlign:"center"}}>
+                    Enter 4 digit code {'\n'}to authenticate
                 </Text>
-            </TouchableOpacity>
+                <Text style={{color:"#000", fontSize:16, textAlign:"center"}}>
+                     Check your email
+                </Text>
+            </View>
+            <View style={styles.ContainerView}>
+                <Text style={{color:"#fff", fontSize:48, textAlign:"center"}}>
+                    0000
+                </Text>                
+            </View>
+            <View style={styles.ContainerView}>
+                <Text style={{color:"#fff", fontSize:48, textAlign:"center"}}>
+                    0000
+                </Text>                
+            </View>
+            {/* <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>
+                    Forgot password page
+                </Text>
+                <TouchableOpacity  onPress={() => navigation.navigate('EmailVerification')}>
+                    <Text>
+                        NEXT PAGE
+                    </Text>
+                </TouchableOpacity>
+            </View> */}
         </View>
+        </>
     )
 }
+
+
+const styles = StyleSheet.create({
+    mainContainer: {
+      flex: 1,
+      backgroundColor: '#9493AD',  
+    },
+    ContainerView:{
+        flex:2,
+        justifyContent:"center",
+        alignItems:"center",
+    },
+  
+})
