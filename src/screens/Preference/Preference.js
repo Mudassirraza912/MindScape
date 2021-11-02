9501008fcfffe7c8d60633f90f31de5adbc4fcf8
Asim-Ur-Rehman <asim.rehman@techstirr.com>
2021-11-02 16:56:47 +0500

    drawer screen

---
+++ src/screens/Preference/Preference.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const Preference = () => {
+    return (
+        <View>
+            <Text>  Prferegygygyf</Text>
+        </View>
+    )
+}
+
+
